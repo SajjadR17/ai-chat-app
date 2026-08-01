6eb9789ca454938b9808aec35e9ebc42073fe23c
Sajjad Roohandeh <Roohandehsredi6@gmail.com>
2026-08-01 15:24:32 +0330

    Mini update

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -92,9 +92,7 @@ function ChatPage() {
   };
 
   useEffect(() => {
-    bottomRef.current?.scrollIntoView({
-      behavior: "smooth",
-    });
+    scrollToBottom()
   }, [messages]);
 
   const copyMessage = async (id, text) => {
@@ -172,7 +170,7 @@ function ChatPage() {
         {chatId === "new" ? (
           <div className="empty-state">
             <div className="empty-state-dot"></div>
-            <h2>Hi {userProfile.username}</h2>
+            <h2>Hi {userProfile?.username}</h2>
             <span>Send a message below so Nightline will reply</span>
           </div>
         ) : (
